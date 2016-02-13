73fa0c0cd9a0d10368ae9d438a88602a1851af3b
@m!r <amir.email@yahoo.com>
2016-02-13 15:41:44 +0200

    change FileUploadTrait.php

--- app/Http/Controllers/Traits/FileUploadTrait.php
+++ app/Http/Controllers/Traits/FileUploadTrait.php
@@ -1,5 +1,6 @@
 <?php namespace App\Http\Controllers\Traits;
 
+use File;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 
@@ -10,18 +11,26 @@ trait FileUploadTrait
      */
     public function saveFiles(Request $request)
     {
-        if (!file_exists(public_path('uploads'))) {
-            mkdir(public_path('uploads'), 0777);
-            mkdir(public_path('uploads/thumb'), 0777);
+        /**
+         * Get Controller Name for uploade folder
+         */
+        $action      = app('request')->route()->getAction();
+        $controller  = class_basename($action['controller']);
+        $controller  = explode('@', $controller);
+        $folder_name = strtolower(str_replace('Controller', '/', $controller[0]));
+
+        if (!file_exists(base_path('uploads/') . $folder_name)) {
+            mkdir(base_path('uploads/') . $folder_name, 0777);
+            mkdir(base_path('uploads/thumb/') . $folder_name, 0777);
         }
         foreach ($request->all() as $key => $value) {
             if ($request->hasFile($key)) {
                 if ($request->has($key . '_w') && $request->has($key . '_h')) {
                     // Check file width
-                    $filename = time() . '-' . $request->file($key)->getClientOriginalName();
+                    $filename = $folder_name . time() . '-' . $request->file($key)->getClientOriginalName();
                     $file     = $request->file($key);
                     $image    = Image::make($file);
-                    Image::make($file)->resize(50, 50)->save(public_path('uploads/thumb') . '/' . $filename);
+                    Image::make($file)->resize(50, 50)->save(base_path('uploads/thumb') . '/' . $filename);
                     $width  = $image->width();
                     $height = $image->height();
                     if ($width > $request->{$key . '_w'} && $height > $request->{$key . '_h'}) {
@@ -35,16 +44,29 @@ trait FileUploadTrait
                             $constraint->aspectRatio();
                         });
                     }
-                    $image->save(public_path('uploads') . '/' . $filename);
+                    $image->save(base_path('uploads') . '/' . $filename);
                     $request->replace(array_merge($request->all(), [$key => $filename]));
                     $request->files->remove($key);
                 } else {
-                    $filename = time() . '-' . $request->file($key)->getClientOriginalName();
-                    $request->file($key)->move(public_path('uploads'), $filename);
+                    $filename = $folder_name . time() . '-' . $request->file($key)->getClientOriginalName();
+                    $request->file($key)->move(base_path('uploads'), $filename);
                     $request->replace(array_merge($request->all(), [$key => $filename]));
                     $request->files->remove($key);
                 }
+                // Check & Delete current file
+                if ($request->has($key . '_c')) {
+                    $this->deleteFiles($request->{$key . '_c'});
+                }
             }
         }
+    }
+
+    /**
+     * File delete trait used in controllers to upload files
+     */
+    public function deleteFiles($filename)
+    {
+        if (File::exists(base_path('uploads/thumb/') . $filename)) {File::delete(base_path('uploads/thumb/') . $filename);}
+        if (File::exists(base_path('uploads/') . $filename)) {File::delete(base_path('uploads/') . $filename);}
     }
 }
