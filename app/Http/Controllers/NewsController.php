0bff83c291b00d411c3d7ae445d00468c2ba41b5
@m!r <amir.email@yahoo.com>
2016-03-14 23:38:03 +0200

    How to use external classes and PHP files in Laravel Controller?
    persian_normalizer
    extra_slug

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\News;
-use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class NewsController extends Controller
 {
@@ -16,30 +16,29 @@ class NewsController extends Controller
     public function index()
     {
 /*        $news = News::where('language', '=', app()->getLocale() )
-                    ->orderby('published','desc')
-                    ->get(['items_id',
-                        'keywords',
-                        'originId',
-                        'title',
-                        'published',
-                        'content',
-                        'visual_url',
-                        'direction',
-                        'language']);*/
+->orderby('published','desc')
+->get(['items_id',
+'keywords',
+'originId',
+'title',
+'published',
+'content',
+'visual_url',
+'direction',
+'language']);*/
 
-        $news = News::where('language', '=', app()->getLocale() )
-                    ->orderby('published','desc')
-                    ->select(['items_id',
-                        'keywords',
-                        'originId',
-                        'title',
-                        'published',
-                        'content',
-                        'visual_url',
-                        'direction',
-                        'language'])
-                    ->paginate(3);
-
+        $news = News::where('language', '=', app()->getLocale())
+            ->orderby('published', 'desc')
+            ->select(['items_id',
+                'keywords',
+                'originId',
+                'title',
+                'published',
+                'content',
+                'visual_url',
+                'direction',
+                'language'])
+            ->paginate(3);
 
         return view('favorites', compact('news'));
     }
@@ -52,18 +51,30 @@ class NewsController extends Controller
      */
     public function show($slug)
     {
-        $news = News::where('language', '=', app()->getLocale() )
-                    ->select(['items_id',
-                        'keywords',
-                        'originId',
-                        'title',
-                        'published',
-                        'content',
-                        'visual_url',
-                        'direction',
-                        'language'])
-                    ->first(3);
+        $news = News::where('language', '=', app()->getLocale())
+            ->select(['items_id',
+                'keywords',
+                'originId',
+                'title',
+                'published',
+                'content',
+                'visual_url',
+                'direction',
+                'language'])
+            ->first(3);
         // $news = 'test';
+        $separator = '-';
+        $string    = 'استراتژی احمدی‌نژاد برای بازگشت به قدرت/ واکنش هاشمی به کوفی خواندن تهرانی‌ها/ بهترین هدیه دوران ریاست جمهوری روحانی/ اتفاق بزرگ مجلس دهم؟';
+        $string .= '3123123٤٥٦/;,';
+        $string .= 'عربي ';
+        $string .= 'زمینه‌ها ';
+        $string .= ' başlık ülçü';
+        $string .= ' Yazılım geliştiricilere en yüksek maaşı veren 20 ‘unicorn’ Glassdoor’un Kasım 2015’te yayınladığı rapora ';
+
+
+        $str = persian_normalizer($string);
+        $str = extra_slug($str);
+
 
         return view('favorite', compact('news'));
     }
